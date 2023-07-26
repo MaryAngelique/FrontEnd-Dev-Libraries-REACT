c547bce6791abeca5641d100d1903e58782e8d31
Angelique Abacajan <ma_utsey@yahoo.com>
2023-07-26 12:56:49 -0400

    Implemented Create a React Component

---
+++ Create a React Component/index.jsx
@@ -0,0 +1,15 @@
+class MyComponent extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        // Changed code below this line
+        return (
+            <div>
+                <h1>Hello React!</h1>
+            </div>
+        );
+        // Changed code above this line
+    }
+}
